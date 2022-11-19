ee76f02e6095aa6212643cb7483ef667c57779c4
Dennis Gove <dpgove@gmail.com>
2022-11-19 18:30:43 -0500

    Initial Commit

---
+++ battlesim/battlesimApp.swift
@@ -0,0 +1,17 @@
+//
+//  battlesimApp.swift
+//  battlesim
+//
+//  Created by Dennis Gove on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct battlesimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
